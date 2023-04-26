cffca4caae99aae517b737aaa521e0dd823ea818
DiTzy21 <hadi13november@gmail.com>
2023-04-26 21:14:30 +0700

    login and register page done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,12 @@
-<!-- @extends('layouts.app') -->
 
-@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Daftar Akun &mdash; UANGKU</title>
-    <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}">
+    <link rel="shortcut icon" href="{{ url('img/logo.png') }}">
+    <!-- <img src="{{url('images/logo2.png')}}"> -->
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
@@ -133,4 +132,4 @@
 <script src="{{ asset('assets/js/custom.js') }}"></script>
 </body>
 </html>
-@endsection
+
